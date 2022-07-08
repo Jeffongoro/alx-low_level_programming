c65a36209fc307ce78110ff2a87aa1e339601b3e
Jeffongoro <ongorogc@gmail.com>
2022-07-08 04:44:06 -0700

    nice

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ *  * print_number - a function that prints an integer
+ *
+ *   * @n: input
+ *
+ *    * Return: inputted integer
+ *
+ *     */
+
+void print_number(int n)
+
+{
+
+
+
+		return (0);
+
+}
+
+
