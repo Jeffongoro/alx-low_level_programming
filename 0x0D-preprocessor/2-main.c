c030885d35a943e72cbb2b042e7f01329dcba3aa
Jeffongoro <ongorogc@gmail.com>
2022-08-02 00:10:43 -0700

    done

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+/**
+ *
+ *  * main - entrypoint
+ *
+ *   *
+ *
+ *    * Return: always 0
+ *
+ *     */
+
+
+
+
+
+int main(void)
+
+{
+
+		printf("%s\n", __FILE__);
+
+			return (0);
+
+}
